4e171e64f82061d8fd60e3883292fe51437d90d8
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2020-02-18 20:53:09 +0300

    Refactored

--- lib/question.dart
+++ lib/question.dart
@@ -1,8 +1,5 @@
 class Question {
   String questionText;
   bool questionAnswer;
-  Question({ String questionText, bool questionAnswer }) {
-    this.questionText = questionText;
-    this.questionAnswer = questionAnswer;
-  }
+  Question({ this.questionText, this.questionAnswer });
 }
